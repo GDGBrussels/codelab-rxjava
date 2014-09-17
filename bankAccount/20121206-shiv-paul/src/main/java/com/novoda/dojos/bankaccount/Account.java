4f7087d86f514a0f65f1044943021a64896a626f
Friedger Müffke <friedger@gmail.com>
2014-09-17 09:19:24 +0200

    remove other dirs

--- bankAccount/20121206-shiv-paul/src/main/java/com/novoda/dojos/bankaccount/Account.java
+++
@@ -1,9 +0,0 @@
-package com.novoda.dojos.bankaccount;
-
-import com.novoda.dojos.bankaccount.domain.Money;
-
-public interface Account {
-	void add(Money money);
-	void remove(Money money);
-	Money getBalance();
-}
--- bankAccount/20121206-shiv-paul/src/main/java/com/novoda/dojos/bankaccount/logging/Logger.java
+++
@@ -1,7 +0,0 @@
-package com.novoda.dojos.bankaccount.logging;
-
-public interface Logger<L, O, G> {
-
-	void log(L arg1, O arg2, G arg3);
-
-}
